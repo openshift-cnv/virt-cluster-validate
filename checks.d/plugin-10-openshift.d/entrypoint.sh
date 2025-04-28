52324e01e0d9ee9006460bb26d61cc9339608ddc
Fabian Deutsch <fabiand@fedoraproject.org>
2025-04-28 13:51:03 +0200

    plugins: Add parallelism
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/plugin-10-openshift.d/entrypoint.sh
+++ checks.d/plugin-10-openshift.d/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
-set -x
-
 export PLUGIN_DISPLAYNAME="OpenShift"
 
 main() {
+    set -m
     for STEP in $(ls -1d ??-* | sort);
     do
+        (
         export CHECK_NAME=$STEP
         $STEP/check.sh
+        ) &
     done
+    wait -f
 }
 
 ping() { echo pong from $PLUGIN_NAME; }
--- checks.d/plugin-50-openshift-virtualization.d/entrypoint.sh
+++ checks.d/plugin-50-openshift-virtualization.d/entrypoint.sh
@@ -3,11 +3,15 @@
 PLUGIN_NAME="OpenShift Virtualization"
 
 main() {
+    set -m
     for STEP in $(ls -1d ??-* | sort);
     do
+        (
         export CHECK_NAME=$STEP
         $STEP/check.sh
+        ) &
     done
+    wait -f
 }
 
 ping() { echo pong from $PLUGIN_NAME; }
