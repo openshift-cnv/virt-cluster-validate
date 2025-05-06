a2f0287d0a21d7cf5dfd26aaba1799d2a9fc2040
Fabian Deutsch <fabiand@fedoraproject.org>
2025-05-06 13:52:26 +0200

    Check auth
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/plugin-10-openshift.d/00-installation.d/check.sh
+++ checks.d/plugin-10-openshift.d/00-installation.d/check.sh
@@ -7,6 +7,8 @@ export CHECK_DISPLAYNAME="Installation"
 run() {
   oc get projects | grep openshift-cnv \
   || fail_with Availability "OpenShift Virtualization does not seem to be present. Did you install the OpenShift Virtualization operator?"
+
+  oc auth can-i list nodes || fail_with Auth "You can not list nodes. Are you bound to the cluster-reader role? This is required by this tool."
 }
 
 cleanup() {
