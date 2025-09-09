db306745d660c573ae4aa64a249207233e7e506b
Fabian Deutsch <fabiand@fedoraproject.org>
2025-09-09 08:22:20 +0200

    PROGRESS
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/lib.sh
+++ checks.d/lib.sh
@@ -23,38 +23,6 @@ green() { echo -e "\e[0;32m$@\e[0m" ; }
 die() { red "FATAL: $@" ; exit 1 ; }
 assert() { echo "(assert:) \$ $@" ; eval $@ || { echo "(assert?) FALSE" ; die "Assertion ret 0 failed: '$@'" ; } ; green "(assert?) True" ; }
 
-#
-# Return results
-#
-append_result_json() {
-    [[ -z "$PLUGIN_NAME" ]] && die "PLUGIN_NAME must be set"
-    [[ -z "$CHECK_NAME" ]] && die "CHECK_NAME must be set"
-    [[ -z "$CHECK_DISPLAYNAME" ]] && die "CHECK_DISPLAYNAME must be set"
-    [[ "$PASS" =~ true|false ]] || die "pass must be true or false"
-    [[ "$LVL" =~ INFO|WARN|ERR ]] || die "level must be one of INFO WARN ERR"
-    cat <<EOJ | { if [[ -z "$STEP" ]]; then jq "del(.step)"; else cat; fi  } >> $RESULTFILE
-{
-  "plugin": {
-    "name": "$PLUGIN_NAME",
-    "displayname": "$PLUGIN_DISPLAYNAME",
-    "image": "$PLUGIN_IMAGE_URL"
-  },
-  "check": {
-    "name": "$CHECK_NAME",
-    "displayname": "$CHECK_DISPLAYNAME",
-    "message": "$MESSAGE",
-    "pass": ${CHECK_PASS:-true}
-  },
-  "step": {
-    "name": "$STEP",
-    "pass": $PASS,
-    "level": "$LVL",
-    "message": "$MESSAGE"
-  }
-}
-EOJ
-}
- 
 pass()           { STEP=""         ; PASS=true  LVL="INFO" MESSAGE="$@" ; append_result_json ; }
 pass_with_info() { STEP=$1 ; shift ; PASS=true  LVL="INFO" MESSAGE="$@" ; append_result_json ; }
 pass_with_warn() { STEP=$1 ; shift ; PASS=true  LVL="WARN" MESSAGE="$@" ; append_result_json ; }
