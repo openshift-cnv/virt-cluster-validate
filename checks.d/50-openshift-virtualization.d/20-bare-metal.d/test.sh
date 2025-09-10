2b530a02989b93ce8288be48e5abed62cac61ce7
Fabian Deutsch <fabiand@fedoraproject.org>
2025-09-10 11:18:48 +0200

    Add IT
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/50-openshift-virtualization.d/20-bare-metal.d/test.sh
+++ checks.d/50-openshift-virtualization.d/20-bare-metal.d/test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 INFRA=$(oc get infrastructure cluster -o json | jq -re '.spec.platformSpec.type')
+INFRA_INSTANCE_TYPES=$(oc get nodes -o json | jq -re '.items[] | .metadata.labels["node.kubernetes.io/instance-type"]' | sort -u)
 
 case "$INFRA" in
   BareMetal|None) pass_with info Infrastructure "Platform '$INFRA'" ;;
-               *) fail_with Infrastructure "Platform '$INFRA'. This does not look like it is bare metal." ;;
+               *) fail_with Infrastructure "Platform '$INFRA' with instance type '$INFRA_INSTANCE_TYPE'. This does not look like it is bare metal." ;;
 esac
+
+node.kubernetes.io/instance-type
