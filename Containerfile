086771de57daebc8b9e8be1401d60bdf0d2a5059
Fabian Deutsch <fabiand@fedoraproject.org>
2025-09-11 13:21:49 +0200

    run from container
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- Containerfile
+++ Containerfile
@@ -2,6 +2,20 @@ FROM fedora
 
 RUN dnf install -y jq
 
+ENV APP=virt-cluster-validate
+
+ENV WD="/app"
+ENV HOME="$WD"
+ENV PATH="$PATH:/app/bin"
+
+ENV RESULTSD="/results.d"
+ENV NUM_CONCURRENT_TESTS="42"
+ENV SINGLE_TEST_TIMEOUT="5m"
+ENV FILTER_ON=".*"
+
+ENV DEFAULT_IMAGE="quay.io/openshift-virtualization/$APP"
+
 ENTRYPOINT ["/app/bin/entrypoint.sh"]
 
+RUN mkdir $RESULTSD
 ADD . /app
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/bash
 
-export PATH=$PATH:/app/bin
-
-if [[ "$1" = "podman-args" ]];
-then
-# arguments for podman
 cat <<EOC
-      --env WD=/ \
-      --env HOME=/ \
-      --env RESULTSD=/results.d/ \
-      --env NUM_CONCURRENT_TESTS=42 \
-      --env SINGLE_TEST_TIMEOUT=5m \
-      --env TEST_FILTER="\$PLUGIN_FILTER" \
-      --volume \${RESULTSD:-\$PWD/results.d/}:/results.d:rw,z \
-      --volume \$HOME/.kube:/.kube:ro,z \
-      --volume \$(which oc):/usr/bin/oc:ro,bind,exec,z \
-      --volume \$(which virtctl):/usr/bin/virtctl:ro,bind,exec,z
+# Paste the following command into a shell
+podman -r run --rm \
+--volume \$HOME/.kube:/app/.kube:ro,z \
+--volume \$(which oc):/usr/bin/oc:ro,bind,exec,z \
+--volume \$(which virtctl):/usr/bin/virtctl:ro,bind,exec,z \
+--entrypoint bash \
+$DEFAULT_IMAGE \
+-c "testrunner ; summarize_results"
 EOC
-  exit 0
-else
-  testrunner
-  summarize_results "$RESULTSD"
-fi
