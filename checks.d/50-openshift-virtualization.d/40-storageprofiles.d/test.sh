24ab6d20d6e8169fecfe2d2e383da3e1ae9b012a
Fabian Deutsch <fabiand@fedoraproject.org>
2026-03-19 12:45:41 +0100

    Fix storageprofile check
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/50-openshift-virtualization.d/40-storageprofiles.d/test.sh
+++ checks.d/50-openshift-virtualization.d/40-storageprofiles.d/test.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/bash
 
-oc get storageprofiles -o json > storageprofiles.json || fail_with Basic "No storageclasses found."
+oc get storageprofiles.cdi.kubevirt.io -o json | tee storageprofiles.json || fail_with Basic "No storageclasses found."
 
 # No claimPropertySets, impact: User has ot specify acces and vol mode
 cat storageprofiles.json \
 | jq -e '[ .items[] | select(.status | has("claimPropertySets") | not) | .metadata.name] | length == 0' \
-|| pass_with info Known "Some storage classes are not covered by storage profiles"
+|| pass_with info "Profile - Some storage classes are not covered by storage profiles"
 
 # Clone strategy 'copy', impact: Slow clone
 cat storageprofiles.json \
 | jq -e '[ .items[] | select(.status.cloneStrategy == "copy") | .metadata.name] | length == 0' \
-|| pass_with info Clone "Some storage classes only support dumb cloning, leading to slow cloning and potentially slow VM launch times"
+|| pass_with warn "Dump cloning - Some storage classes only support dumb cloning, leading to slow cloning and potentially slow VM launch times"
 
 cat storageprofiles.json \
 | jq -e '[ .items[] | .status.claimPropertySets[]?.accessModes ] | unique | flatten | index ("ReadWriteMany")' \
-|| pass_with warn ReadWriteMany "There is no storageclass supporting ReadWriteMany, Live Migraiton will not be possible."
+|| pass_with warn "RWX - There is no storageclass supporting ReadWriteMany, Live Migraiton will not be possible."
 
 cat storageprofiles.json \
 | jq -e '[ .items[] | .status.claimPropertySets[]?.volumeMode ] | unique | flatten | index ("Block")' \
-|| pass_with info ReadWriteMany "There is now storageclass supporting Block mode, this can lead to lower performance."
+|| pass_with info "Block - There is now storageclass supporting Block mode, this can lead to lower performance."
 
 #jq '[ .items[] | select(.status | has("cloneStrategy") | not) | .metadata.name] | length'
