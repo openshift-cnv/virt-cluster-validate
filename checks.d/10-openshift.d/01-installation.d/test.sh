bb2b7065d032085a5b285a7829e72144e8569cb0
Fabian Deutsch <fabiand@fedoraproject.org>
2026-03-19 12:55:05 +0100

    Move login check to 10-openshift.d and reorder installation check

---
+++ checks.d/10-openshift.d/01-installation.d/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+step Connectivity
+oc whoami
+oc whoami --show-server
+oc get project || fail_with "Unable to retrieve projects from cluster"
+
+step Availability
+oc get projects | grep openshift-cnv \
+|| fail_with "OpenShift Virtualization does not seem to be present. Did you install the OpenShift Virtualization operator?"
+
+step Auth
+oc auth can-i list nodes || fail_with "You can not list nodes. Are you bound to the cluster-reader role? This is required by this tool."
