dcb9eae60fdc42c858e83763cd90af71c7529fac
Fabian Deutsch <fabiand@fedoraproject.org>
2026-07-17 15:23:13 +0200

    fix multi line
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- checks.d/50-openshift-virtualization.d/10-quota.d/test.sh
+++ checks.d/50-openshift-virtualization.d/10-quota.d/test.sh
@@ -15,5 +15,7 @@
 # limitations under the License.
 #
 
+set -e
+
 [[ "$(oc get quota -o name | wc -l)" -eq 0 ]] \
 || pass_with warn "There is a quota set on the namespace, this can break this validation. Please remove the quota if any test fails, and retry."
